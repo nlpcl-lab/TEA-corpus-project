733267d6f981de81f4960646b4d16cc8623b3e49
Junseop <gaon0403@gmail.com>
2019-11-14 21:24:18 +0900

    Add entailment-acceptability relation to stats.py

--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ import jsonlines
 
 from tqdm import tqdm
 from statistics import mode
-from collections import Counter
+from collections import Counter, defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--jsonl', type=str, default='./jsonls/TEA.jsonl')
@@ -37,8 +37,39 @@ def get_ent_label_distribution(jsons):
            for i in ent_counter])
 
 
+def MAPE(reals, predictions):
+    assert len(reals) == len(predictions)
+
+    return 100 / len(reals) * sum(
+        [abs(real - pred) / real for real, pred in zip(reals, predictions)])
+
+
+def filtered(lst):
+    return list(filter(lambda x: x > 0, lst))
+
+
+def ent_acc_relation(jsons):
+    ent_dict = defaultdict(lambda: [list(), list()])
+    for json in jsons:
+        try:
+            label = ENT_LABEL[mode(json['ent'])]
+            ent_dict[label][0].append(
+                sum(filtered(json['acc'][0])) /
+                len(filtered(json['acc'][0])))
+            ent_dict[label][1].append(
+                sum(filtered(json['acc'][1])) /
+                len(filtered(json['acc'][1])))
+        except:
+            continue
+
+    print('===Entailment-Acceptability Relation===')
+    for key in ENT_LABEL:
+        print('%s: %.3f%%' % (key, MAPE(ent_dict[key][0], ent_dict[key][1])))
+
+
 if __name__ == '__main__':
     reader = jsonlines.open(args.jsonl)
     jsons = [x for x in reader]
 
     get_ent_label_distribution(jsons)
+    ent_acc_relation(jsons)
